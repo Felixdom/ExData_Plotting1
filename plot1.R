cc2da9b9d7abab07bd19a010e2e4b33e7c797d05
Felixdom <felix.domenech@gmail.com>
2015-08-09 23:57:38 +0200

    Create plot1.R

---
+++ plot1.R
@@ -0,0 +1,20 @@
+if(!file.exists("./Electric")){dir.create("./Electric")}
+unzip("household_power_consumption.zip")
+
+library(dplyr)
+data<-read.table("household_power_consumption.txt",header=TRUE,nrows=70000,sep=";",
+                 na.strings="?")
+
+##Convert the Date variable to Date classes in R.
+data$Date<-as.Date(data$Date,"%d/%m/%Y")
+
+##Select two days
+datos<-filter(data,Date=="2007-02-01"| Date=="2007-02-02")
+
+##Make histogram
+with(datos,hist(Global_active_power,col="red",main="Global Active Power",
+                xlab="Global Active Power (kilowatts)"))
+
+##Copy the histogram in a png file:
+dev.copy(png, file="plot1.png")
+dev.off()
