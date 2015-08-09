c3818f28ef185e33eadd58454efb15a5db94aa69
Felixdom <felix.domenech@gmail.com>
2015-08-10 01:05:05 +0200

    Update plot3.R

--- plot3.R
+++ plot3.R
@@ -7,7 +7,7 @@ data<-read.table("household_power_consumption.txt",header=TRUE,nrows=70000,sep="
 ##Convert the Date variable to Date classes in R.
 data$Date<-as.Date(data$Date,"%d/%m/%Y")
 
-##Select two days
+##Select two days.
 datos<-filter(data,Date=="2007-02-01"| Date=="2007-02-02")
 
 ##Make plot
